8b11e462f70553d819ce78d5bf1064aa7a213043
Dipti <pangam.dipti.99@gmail.com>
2021-04-23 11:29:17 +0530

    First Commit

---
+++ GitHubTestProject/src/com/neosoft/githubtest/GitHubTest.java
@@ -0,0 +1,14 @@
+package com.neosoft.githubtest;
+
+public class GitHubTest {
+
+	public static void main(String[] args) {
+		String code = "SE";
+		if (code == "SE")
+			System.out.println("Software Engineer");
+		else
+			System.out.println("Not a Software Engineer");
+
+	}
+
+}
